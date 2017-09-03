aedaa32859ed6e415e2051d15803d58b5bdba1c4
Maxou <baconn_m@epitech.eu>
2017-09-03 11:21:05 +0200

    Fix websockets

--- app.js
+++ app.js
@@ -4,15 +4,12 @@ const cors = require('cors');
 
 // Customs requires
 const routes = require('./routes/routes');
-const proxy = require('./core/proxy');
+//const proxy = require('./core/proxy');
 
 // Init Express App
 const app = express();
 app.use(cors());
 app.use('/', routes);
-app.on('upgrade', (req, socket, head) => {
-	proxy.ws(req, socket, head);
-});
 
 // Export app
 module.exports = app;
