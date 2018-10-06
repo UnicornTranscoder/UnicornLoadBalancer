ebe37a5bb94238ff752964024eb91ac035eadaf3
Maxime Baconnais <m.baconnais@outlook.com>
2018-10-06 17:06:33 +0200

    Support different IP for a same serveur (To fix peering issues)

--- core/serverManager.js
+++ core/serverManager.js
@@ -2,9 +2,13 @@
  * Created by Maxime Baconnais on 29/08/2017.
  */
 
+const geoip = require('geoip-lite');
 const Netmask = require('netmask').Netmask;
 const config = require('../config');
 const stats = require('../core/stats');
+
+// Hot reload geoip
+geoip.startWatchingDataUpdate();
 
 let serverManager = {}
 
@@ -13,52 +17,52 @@ serverManager.sessions = {};
 serverManager.stoppedSessions = {};
 
 serverManager.saveSession = (req) => {
-    if (typeof(req.query['X-Plex-Session-Identifier']) !== 'undefined' && typeof(req.query.session) !== 'undefined') {
-        serverManager.cacheSession[req.query['X-Plex-Session-Identifier']] = req.query.session.toString();
-    }
+	if (typeof (req.query['X-Plex-Session-Identifier']) !== 'undefined' && typeof (req.query.session) !== 'undefined') {
+		serverManager.cacheSession[req.query['X-Plex-Session-Identifier']] = req.query.session.toString();
+	}
 };
 
 serverManager.calculateServerLoad = (stats) => {
 	// The configuration is unavailable, the server is probably unavailable
 	if (!stats)
 		return (1000);
-	
+
 	// Default load 0
 	let load = 0;
-	
+
 	// Each transcode add 1 to the load
 	load += stats.transcoding;
-	
+
 	// Each HEVC sessions add 1.5 to the load
 	if (stats.codecs.hevc)
 		load += stats.codecs.hevc * 1.5;
-	
+
 	// Server already have too much sessions
 	if (stats.config && stats.sessions >= stats.config.preferredMaxSessions)
 		load += 2.5;
-	
+
 	// Server already have too much transcodes
 	if (stats.config && stats.transcoding >= stats.config.preferredMaxTranscodes)
 		load += 5;
-	
+
 	// Server already have too much downloads
 	if (stats.config && stats.downloads >= stats.config.preferredMaxDownloads)
 		load += 1;
-	
+
 	// Return load
 	return (load);
 }
 
 serverManager.getSession = (req) => {
-	if (typeof(req.params.sessionId) !== 'undefined')
+	if (typeof (req.params.sessionId) !== 'undefined')
 		return (req.params.sessionId);
-	if (typeof(req.query.session) !== 'undefined')
+	if (typeof (req.query.session) !== 'undefined')
 		return (req.query.session);
-	if (typeof(req.query['X-Plex-Session-Identifier']) !== 'undefined' && typeof(serverManager.cacheSession[req.query['X-Plex-Session-Identifier']]) !== 'undefined')
+	if (typeof (req.query['X-Plex-Session-Identifier']) !== 'undefined' && typeof (serverManager.cacheSession[req.query['X-Plex-Session-Identifier']]) !== 'undefined')
 		return (serverManager.cacheSession[req.query['X-Plex-Session-Identifier']]);
-	if (typeof(req.query['X-Plex-Session-Identifier']) !== 'undefined')
+	if (typeof (req.query['X-Plex-Session-Identifier']) !== 'undefined')
 		return (req.query['X-Plex-Session-Identifier']);
-	if (typeof(req.query['X-Plex-Client-Identifier']) !== 'undefined')
+	if (typeof (req.query['X-Plex-Client-Identifier']) !== 'undefined')
 		return (req.query['X-Plex-Client-Identifier']);
 	return (false);
 };
@@ -74,32 +78,46 @@ serverManager.forceStopStream = (session, reason) => {
 	serverManager.stoppedSessions[session] = reason;
 };
 
+serverManager.geoIp = (url, ip) => {
+
+	const geo = geoip.lookup(ip);
+	console.log(ip, geo);
+
+	if (typeof (stats[url].routing) !== 'undefined') {
+		if (typeof (stats[url].routing[geo.country + '-' + geo.region])) // 'COUNTRY-REGION' tag like 'US-TX'
+			return (stats[url].routing[geo.country + '-' + geo.region])
+		if (typeof (stats[url].routing[geo.country])) // 'COUNTRY' tag like 'US'
+			return (stats[url].routing[geo.country])
+	}
+	return (url);
+}
+
 serverManager.chooseServer = (session, ip = false) => {
-	
+
 	//Pre-prod
 	if (config.preprod.enabled && ip) {
-        for (let i = 0; i < config.preprod.devIps.length; i++) {
-            let mask = new Netmask(config.preprod.devIps[i]);
+		for (let i = 0; i < config.preprod.devIps.length; i++) {
+			let mask = new Netmask(config.preprod.devIps[i]);
 
-            if (mask.contains(ip))
-                return config.preprod.server;
-        }
+			if (mask.contains(ip))
+				return config.preprod.server;
+		}
 	}
-	
+
 	let count = config.cluster.length;
 	if (count == 0)
 		return (false);
-	if (typeof(serverManager.sessions[session]) !== 'undefined' &&
+	if (typeof (serverManager.sessions[session]) !== 'undefined' &&
 		config.cluster.indexOf(serverManager.sessions[session]) != -1 &&
 		stats[serverManager.sessions[session]]) {
-		return (serverManager.sessions[session]);
+		return (serverManager.geoIp(serverManager.sessions[session], ip));
 	}
-	
-	let sortedServers = config.cluster.sort((url) => { return (serverManager.calculateServerLoad(stats[url]));});
-		
+
+	let sortedServers = config.cluster.sort((url) => { return (serverManager.calculateServerLoad(stats[url])); });
+
 	serverManager.sessions[session] = sortedServers[0];
-	
-	return (sortedServers[0]);
+
+	return (serverManager.geoIp(sortedServers[0], ip));
 };
 
 serverManager.removeSession = (session) => {
