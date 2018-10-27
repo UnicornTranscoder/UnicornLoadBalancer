1645ef04f990ced4adeb60e51068dd145e4f593a
Benjamin DROUARD <benjamin.drouard@epitech.eu>
2018-10-27 12:48:52 +0200

    Improvements

--- src/core/sessions.js
+++ src/core/sessions.js
@@ -1,4 +1,5 @@
 import debug from 'debug';
+import uniqid from 'uniqid';
 
 import config from '../config';
 import { publicUrl, plexUrl, redis } from '../utils';
@@ -7,6 +8,93 @@ import { publicUrl, plexUrl, redis } from '../utils';
 const D = debug('UnicornLoadBalancer:SessionsManager');
 
 let SessionsManager = {};
+
+let sessions = [
+    {
+        unicorn: '_UNICORN',
+        session: '',
+        sessionId: '',
+        sessionFull: '',
+        sessionIdentifier: '',
+        clientIdentifier: '',
+        args: [],
+        env: [],
+        pingUrl: ''
+    }
+];
+
+// Parse request to extract session information
+SessionsManager.parseSessionFromRequest = (req) => {
+    const unicorn = (typeof (req.query.unicorn) !== 'undefined') ? { unicorn: req.query.unicorn } : false;
+    const sessionId = (typeof (req.params.sessionId) !== 'undefined') ? { sessionId: req.params.sessionId } : false;
+    const session = (typeof (req.query.session) !== 'undefined') ? { session: req.query.session } : false;
+    const sessionIdentifier = (typeof (req.query['X-Plex-Session-Identifier']) !== 'undefined') ? { "X-Plex-Session-Identifier": req.query['X-Plex-Session-Identifier'] } : false;
+    const clientIdentifier = (typeof (req.query['X-Plex-Client-Identifier']) !== 'undefined') ? { "X-Plex-Client-Identifier": req.query['X-Plex-Client-Identifier'] } : false;
+    return {
+        ...unicorn,
+        ...sessionId,
+        ...session,
+        ...sessionIdentifier,
+        ...clientIdentifier
+    }
+};
+
+// Get a session from its values
+SessionsManager.getSessionFromRequest = (search) => {
+    const filtered = sessions.filter(e => {
+        let keys = Object.keys(search);
+        for (let i = 0; i < keys.length; i++) {
+            if (e[keys[i]] === search[keys[i]])
+                return (true);
+        }
+        return (false);
+    });
+    if (filtered.length === 0)
+        return (false);
+    return (filtered[0]);
+};
+
+// Get a session position from its values
+SessionsManager.getIdFromRequest = (search) => {
+    for (let idx = 0; idx < sessions.length; idx++) {
+        let keys = Object.keys(search);
+        for (let i = 0; i < keys.length; i++) {
+            if (e[keys[i]] === search[keys[i]])
+                return (idx);
+        }
+    }
+    return (false);
+};
+
+// Update the session stored
+SessionsManager.updateSessionFromRequest = (req) => {
+    const args = SessionsManager.parseSessionFromRequest(req);
+    const search = SessionsManager.getSessionFromRequest(search);
+    const idx = SessionsManager.getIdFromRequest(search);
+
+    if (!search) {
+        sessions.push({
+            ...{
+                unicorn: uniqid(),
+                session: '',
+                sessionId: '',
+                sessionFull: '',
+                sessionIdentifier: '',
+                clientIdentifier: '',
+                args: [],
+                env: [],
+                pingUrl: ''
+            },
+            ...args
+        });
+        return (true);
+    }
+    sessions[idx] = {
+        ...sessions[idx],
+        ...args
+    };
+    return (false);
+};
 
 // Parse FFmpeg parameters with internal bindings
 SessionsManager.parseFFmpegParameters = (args) => {
