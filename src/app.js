099dd31e4eaa0712e2b057d580f611fa7ca3e3da
Maxime Baconnais <m.baconnais@outlook.com>
2018-11-12 21:27:20 +0100

    Fix Websocket

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 
 import config from './config';
 import Router from './routes';
+import Proxy from './routes/proxy';
 import { internalUrl } from './utils';
 
 import debug from 'debug';
@@ -25,7 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.use((err, req, res, next) => {
+app.use((err, _, res) => {
     if (err instanceof SyntaxError && err.status >= 400 && err.status < 500 && err.message.indexOf('JSON'))
         res.status(400).send({ error: { code: 'INVALID_BODY', message: 'Syntax error in the JSON body' } });
 });
@@ -36,6 +37,8 @@ D('Initializing API routes...');
 // Routes
 Router(app);
 
-// Bind and start
-app.listen(config.server.port)
+// Create HTTP server, forward websocket and start
+app.listen(config.server.port).on('upgrade', (req, res) => {
+    Proxy.ws(req, res);
+});
 D('Launched on ' + internalUrl());
--- src/routes/proxy.js
+++ src/routes/proxy.js
@@ -15,4 +15,16 @@ RoutesProxy.plex = (req, res) => {
     return (proxy.web(req, res));
 };
 
+RoutesProxy.ws = (req, res) => {
+    const proxy = httpProxy.createProxyServer({
+        target: {
+            host: config.plex.host,
+            port: config.plex.port
+        }
+    }).on('error', () => {
+        res.status(400).send({ error: { code: 'PROXY_TIMEOUT', message: 'Plex not respond in time, proxy request fails' } });
+    });
+    return (proxy.ws(req, res));
+};
+
 export default RoutesProxy;
